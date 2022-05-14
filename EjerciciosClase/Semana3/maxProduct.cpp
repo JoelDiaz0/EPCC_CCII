74627441a711f45a05398bda89b3249b936c9123
berlydiaz12@gmail.com <berlydiaz12@gmail.com>
2022-05-13 23:56:07 -0500

    Exercise 1

--- EjerciciosClase/Semana3/maxProduct.cpp
+++ EjerciciosClase/Semana3/maxProduct.cpp
@@ -4,58 +4,28 @@
 multiplicando dos números*/
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
-  int num, mayor1, mayor2;
-  vector <int> vec;
+  int mayor1 = 0, mayor2 = 0, N;
 
-  do
+  cout<<"Ingrese la cantidad de numeros que quiere ingresar: "; cin>>N;
+
+  for (int i = 0; i < N; i++)
   {
-    cout<<"Ingrese numeros mayores a cero y termine ingresado -1: " ; cin>>num;  
-    if (num>0)
-      vec.push_back(num);
-    else if (num==-1)
-      break;
-    else
-      cout<<"Ingrese un numero valido!\n";
-  } while (num!=-1);
-
-  mayor1=vec.front();
-  mayor2=vec.back();
-
-  if (vec.size()%2==0)
-  {
-    for (int i=0, j=vec.size()-1; i<=vec.size()/2&&j>=vec.size()/2; i++,j--)
+    int num;
+    cout<<"Ingrese un numero ["<<i+1<<"]: "; cin>>num;
+    if (num > mayor1)
     {
-      if (mayor1<vec.at(i))
-         mayor1=vec.at(i);
-      if (mayor2<vec.at(j))
-         mayor2=vec.at(j);
+      mayor2 = mayor1;
+      mayor1 = num;
+    }
+    if (num > mayor2 && num < mayor1)
+    {
+      mayor2 = num;
     }
   }
-
-  else
-  {
-    for (int i=0, j=vec.size()-1; i<=(vec.size()/2)+1&&j>=vec.size()/2; i++,j--)
-    {
-      if (mayor1<vec.at(i))
-         mayor1=vec.at(i);
-      if (mayor2<vec.at(j))
-         mayor2=vec.at(j);
-    }
-  }
-
-  
-  for (auto i=vec.begin(); i!=vec.end(); i++)
-  {
-    cout<<*i<<" ";
-  }
-
-  cout<<"\nEl mayor numero posible es: "<<mayor1*mayor2<<endl;
-  
-  
+  cout<<"El mayor numero que se puede obtener multiplicando de los valores ingresados es: "<<mayor1*mayor2<<endl;
   return 0;
 }
